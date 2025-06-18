2962318fd4c0f9fcbea7edfdb996d1447216799d
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-06-18 12:57:24 +0530

    add heading

--- src/components/Avatars.jsx
+++ src/components/Avatars.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import avatarImg from '../assets/images/png/avatar-img.png'
 import Description from './common/Description'
+import Heading from './common/Heading'
 
 const Avatars = () => {
   return (
@@ -12,6 +13,9 @@ const Avatars = () => {
             <div className="bg-[#C4C4C4] py-8 md:py-[41px] px-5 min-[1435px]:pl-5.5 min-[1435px]:pr-[49px] min-[1438px]:max-w-[481px] w-full">
               <Description descriptionClass={"font-mono min-[1438px]:max-w-[375px]"} descriptionText={"Collection features rare cc0 traits contributed by guest artists and from projects including:"} spanClass={"!mt-[22]px !max-w-[347px]"} spanText={"XCOPY, Rektguy, Moonbirds, Goblintown, Nouns, Blitmaps, Kristy Glas, Cryptoadz, Eclectic Method, Grillz Gang, Robness, Max Osiris"} spanText2={"and more."} />
             </div>
+          </div>
+          <div className="bg-[#999999] pt-[78px] pb-[169px] pl-[63px] pr-[100px]">
+            <Heading headingClass={"max-w-[1065px]"} headingText={"- Broadside Avatars offer two different licensing models: "} />
           </div>
         </div>
       </div>
