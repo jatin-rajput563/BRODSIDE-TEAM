17b1d30f5c882b9f7f83a5b7f52fedd352cbb117
jatin-rajput563 <jkt3298@gmail.com>
2025-06-18 12:56:16 +0530

    work-on-storytelling-section

--- src/components/Storytelling.jsx
+++ src/components/Storytelling.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-
+import Heading from "./common/Heading";
 const Storytelling = () => {
   return (
     <>
       <div className="min-h-screen pt-[82px]">
         <div className="max-w-[1320px] mx-auto px-3">
-          
+          <Heading headingText="-Starting with Storytelling..." />
+        </div>
+        <div className="flex-wrap">
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
         </div>
       </div>
     </>
