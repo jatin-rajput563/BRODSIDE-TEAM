7bf9ae21acdb5a76dc9485acb8b78a92cc6f1a01
jatin-rajput563 <jkt3298@gmail.com>
2025-06-18 11:09:16 +0530

    adding-notify

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Hero from "./components/Hero";
-import Header from "./components/Header";
 import Journey from "./components/Journey";
 import Avatars from "./components/Avatars";
 import Storytelling from "./components/Storytelling";
@@ -8,11 +7,12 @@ import Team from "./components/Team";
 import Charlie from "./components/Charlie";
 import Partners from "./components/Partners";
 import Footer from "./components/Footer";
+import Notify from "./components/Notify";
 function App() {
   return (
     <>
       <Hero />
-      <Header />
+      <Notify />
       <Journey />
       <Avatars />
       <Storytelling />
