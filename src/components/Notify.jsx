c5bb09881f0d65bdaa63510bbc6ec83d79137347
Nupur Bishnoi <nupurbishnoi03@gmail.com>
2025-06-19 11:07:15 +0530

    updates done

--- src/components/Notify.jsx
+++ src/components/Notify.jsx
@@ -7,14 +7,14 @@ import twitter from '../assets/images/png/twitter-img.png'
 const Notify = () => {
   return (
     <div className='notify-bg bg-cover bg-center bg-no-repeat md:min-h-[409px] xl:pl-[181px] px-4 md:py-20 py-10 max-w-[1920px] mx-auto '>
-      <div className='max-w-[1020px]'>
+      <div className='max-w-[1005px]'>
         <Heading headingText="Are you ready?" headingClass="md:!text-[30px] !text-xl !pb-10" />
-        <div className='gap-[14px] flex max-md:flex-wrap lg:justify-center'>
+        <div className='gap-5 flex max-md:flex-wrap lg:justify-center'>
           <CustomInput inputClass="!bg-[#FEFDFF] !w-full xl:!max-w-[730px] !text-black" />
           <CustomButton btnText="NOTIFY ME" btnClass="!bg-[#7BFFCC] !border-black !border-[5px] !whitespace-nowrap max-md:!w-full" />
         </div>
           <a href='https://x.com/' target='_blank'
-            rel='noopener noreferrer' className='flex items-center pt-10 gap-6'>
+            rel='noopener noreferrer' className='flex items-center mt-10 gap-6'>
             <img
               src={twitter}
               alt="Twitter"
