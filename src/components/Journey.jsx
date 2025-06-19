5ca628cb120ff60e339ca7db81e82cbc31960727
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-06-19 11:05:38 +0530

    change code in journey component

--- src/components/Journey.jsx
+++ src/components/Journey.jsx
@@ -6,14 +6,14 @@ const Journey = () => {
   return (
     <>
       <div className="max-w-[1440px] mx-auto lg:pt-20 md:pt-14 pt-10">
-        <Heading headingText={"-The Journey Ahead..."} />
+        <Heading headingClass={"pl-5 sm:pl-10 md:pl-[56px]"} headingText={"-The Journey Ahead..."} />
         <div className="flex lg:mt-20.5 md:mt-14 mt-10 max-xl:flex-wrap">
           {JOURNEY_DATA.map((item, index) => (
             <div key={index} className="max-xl:mx-auto max-xl:mb-6">
               <img
-                className={`${index === 0 ? 'max-[1480px]:max-w-[476px] w-full pointer-events-none' : 'max-[1480px]:max-w-[483px] w-full pointer-events-none'}`} src={item.img} alt="" />
+                className='w-full pointer-events-none' src={item.img} alt="images" />
               <div className="mt-5 md:mt-10 lg:mt-[50px] px-4">
-                <p className='max-w-[365px] text-[28px] font-normal leading-[96%]'>
+                <p className={`text-[28px] font-normal leading-[96%] ${index === 0 ? 'max-w-[315px]' : 'max-w-[365px]'}`}>
                   <span className='font-extrabold'>{item.time}</span> {item.heading}
                 </p>
                 {index === 0 && (
@@ -21,11 +21,10 @@ const Journey = () => {
                     {item.title}
                   </p>
                 )}
-                <p className='text-2xl ff-courir leading-[120%] max-w-[415px] pt-2.5 md:pt-[30px]'>{item.description}</p>
+                <p className={` text-2xl ff-courir leading-[120%] pt-2.5 md:pt-[30px] ${index === 2 ? 'max-w-[415px]' : ' max-w-[440px]'}`}>{item.description}</p>
               </div>
             </div>
           ))}
-
         </div>
       </div>
     </>
