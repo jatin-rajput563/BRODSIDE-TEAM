ea6e398dcef701effb1b7ecaa1b179497ca0029c
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-06-19 14:44:49 +0530

    solve spacing problem in avtar component

--- src/components/Avatars.jsx
+++ src/components/Avatars.jsx
@@ -8,23 +8,23 @@ const Avatars = () => {
     <>
       <div className="md:mt-15 mt-10  lg:mt-[111px]">
         <div className="max-w-[1440px] mx-auto">
-          <div className="min-[1438px]:!flex">
+          <div className="min-[1440px]:!flex">
             <img
-              className="min-[1438px]:max-w-[960px] w-full max-h-[547px] pointer-events-none"
+              className="min-[1440px]:max-w-[960px] w-full max-h-[547px] pointer-events-none"
               src={avatarImg}
               alt="avatar-img"
             />
-            <div className="bg-[#C4C4C4] py-8 md:py-[41px] px-5 min-[1435px]:pl-5.5 min-[1435px]:pr-[49px] min-[1438px]:max-w-[481px] w-full">
+            <div className="bg-[#C4C4C4] py-8 md:py-[41px] px-5 min-[1435px]:pl-5.5 min-[1435px]:pr-[49px] min-[1440px]:max-w-[481px] w-full">
               <Description
-                descriptionClass="min-[1438px]:max-w-[410px]"
+                descriptionClass="min-[1440px]:max-w-[410px]"
                 descriptionText="Collection features rare cc0 traits contributed by guest artists and from projects including:"
-                spanClass="min-[1438px]:max-w-[347px]"
+                spanClass="min-[1440px]:max-w-[347px]"
                 spanText="XCOPY, Rektguy, Moonbirds, Goblintown, Nouns, Blitmaps, Kristy Glas, Cryptoadz, Eclectic Method, Grillz Gang, Robness, Max Osiris"
               />
               <Description descriptionText="and more." />
             </div>
           </div>
-          <div className="bg-[#999999] px-5 py-10 md:pt-[78px] md:pb-[169px] md:pl-[63px] md:pr-[100px]">
+          <div className="bg-[#999999] px-5 py-10 sm:py-12 md:py-15 lg:py-[78px] xl:pb-[169px] md:px-[55px] lg:pr-[100px]">
             <Heading
               headingClass={"max-w-[1065px] text-5xl lg:!text-[64px]"}
               headingText={
